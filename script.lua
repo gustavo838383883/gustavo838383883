cc973a9cc0ec36c6fd50691023b85ab2d49a4b05
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-10-17 13:12:00 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -103,24 +103,6 @@ for i=1, 128 do
 	end
 end
 
-if disk then
-	local color = disk:Read("Color")
-	if color then
-		color = string.split(color, ",")
-		if color then
-			if tonumber(color[1]) and tonumber(color[2]) and tonumber(color[3]) then
-				color = Color3.new(tonumber(color[1])/255, tonumber(color[2])/255, tonumber(color[3])/255)
-			else
-				color = Color3.new(0, 128/255, 218/255)
-			end
-		else
-			color = Color3.new(0, 128/255, 218/255)
-		end
-	else
-		color = Color3.new(0, 128/255, 218/255)
-	end
-end
-
 local keyboardinput = nil
 local backgroundframe = nil
 
@@ -612,6 +594,27 @@ local function loadmenu(screen, disk)
 		end
 	end)
 end
+
+if disk then
+	local color = disk:Read("Color")
+	if color then
+		color = string.split(color, ",")
+		if color then
+			if tonumber(color[1]) and tonumber(color[2]) and tonumber(color[3]) then
+				color = Color3.new(tonumber(color[1])/255, tonumber(color[2])/255, tonumber(color[3])/255)
+			else
+				color = Color3.new(0, 128/255, 218/255)
+			end
+		else
+			color = Color3.new(0, 128/255, 218/255)
+		end
+	else
+		color = Color3.new(0, 128/255, 218/255)
+	end
+else
+	color = Color3.new(0, 128/255, 218/255)
+end
+
 if screen then
 	if disk then
 		if speaker then
