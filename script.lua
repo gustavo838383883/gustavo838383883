33e4aa864773976350f999cda3adf128d9fb99d5
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-10-17 13:10:28 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -103,20 +103,22 @@ for i=1, 128 do
 	end
 end
 
-local color = disk:Read("Color")
-if color then
-	color = string.split(color, ",")
+if disk then
+	local color = disk:Read("Color")
 	if color then
-		if tonumber(color[1]) and tonumber(color[2]) and tonumber(color[3]) then
-			color = Color3.new(tonumber(color[1])/255, tonumber(color[2])/255, tonumber(color[3])/255)
+		color = string.split(color, ",")
+		if color then
+			if tonumber(color[1]) and tonumber(color[2]) and tonumber(color[3]) then
+				color = Color3.new(tonumber(color[1])/255, tonumber(color[2])/255, tonumber(color[3])/255)
+			else
+				color = Color3.new(0, 128/255, 218/255)
+			end
 		else
 			color = Color3.new(0, 128/255, 218/255)
 		end
 	else
 		color = Color3.new(0, 128/255, 218/255)
 	end
-else
-	color = Color3.new(0, 128/255, 218/255)
 end
 
 local keyboardinput = nil
