6b749b629fd0aaa24913839aec4358f3b1e2f55c
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-10-17 13:37:10 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -103,25 +103,20 @@ for i=1, 128 do
 	end
 end
 
-local success, error = pcall(function()
-	local color = disk:Read("Color")
-end)
-if success then
-	local color = disk:Read("Color")
+local color = disk:Read("Color")
+if color then
+	color = string.split(color, ",")
 	if color then
-		color = string.split(color, ",")
-		if color then
-			if tonumber(color[1]) and tonumber(color[2]) and tonumber(color[3]) then
-				color = Color3.new(tonumber(color[1])/255, tonumber(color[2])/255, tonumber(color[3])/255)
-			else
-				color = Color3.new(0, 128/255, 218/255)
-			end
+		if tonumber(color[1]) and tonumber(color[2]) and tonumber(color[3]) then
+			color = Color3.new(tonumber(color[1])/255, tonumber(color[2])/255, tonumber(color[3])/255)
 		else
 			color = Color3.new(0, 128/255, 218/255)
 		end
 	else
 		color = Color3.new(0, 128/255, 218/255)
 	end
+else
+	color = Color3.new(0, 128/255, 218/255)
 end
 
 local keyboardinput = nil
