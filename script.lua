ca3eabd4b25d57a3efbce072d3cb2602405ee071
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-10-25 14:37:15 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -634,6 +634,18 @@ local function calculator(screen)
 		end
 	end)
 
+	local  button20 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.50, 0, 0.5, 25), Text = "-"})
+	holderframe:AddChild(button20)
+	button20.MouseButton1Down:Connect(function()
+		if not type then
+			number1 = "-"..tostring(number1)
+			part1.Text = number1
+		else
+			number2 = "-"..tostring(number2)
+			part2.Text = number2
+		end
+	end)
+
 	local  button11 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0, 0, 0.4, 25), Text = "CE"})
 	holderframe:AddChild(button11)
 	button11.MouseButton1Down:Connect(function()
