125fcde031e5482cc13f1b07106845923d9fd2e2
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-10-27 15:30:20 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -982,7 +982,7 @@ local function loadmenu(screen, disk)
 				pressed = false
 			end
 		else
-			startui = screen:CreateElement("Frame", {Size = UDim2.new(0.3, 0, 0.5, 0), Position = UDim2.new(0, 0, 0.4, 0)})
+			startui = screen:CreateElement("Frame", {Size = UDim2.new(0.3, 0, 0.6, 0), Position = UDim2.new(0, 0, 0.4, 0)})
 			local scrollingframe = screen:CreateElement("ScrollingFrame", {Size = UDim2.new(1, 0, 1, 0), ScrollBarThickness = 4, CanvasSize = UDim2.new(0, 0, 1, 0)})
 			opendiskreader = screen:CreateElement("TextButton", {Text = "Files", TextScaled = true, Size = UDim2.new(1, 0, 0.15, 0)})
 			opencreatefile = screen:CreateElement("TextButton", {Text = "Create/Overwrite File", TextScaled = true, Size = UDim2.new(1, 0, 0.15, 0), Position = UDim2.new(0, 0, 0.15, 0)})
