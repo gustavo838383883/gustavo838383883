a5716b1169cdc44178147b5d288f7c6d09ab3515
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-10-30 13:03:31 -0300

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,295 @@
+local Screen = GetPartFromPort(1, "TouchScreen")
+
+	local holderframe = Screen:CreateElement("Frame", {Size = UDim2.new(0.7, 0, 0.7, 0), Active = true, Draggable = true})
+	local part1 = Screen:CreateElement("TextLabel", {TextScaled = true, Size = UDim2.new(0.45, 0, 0.1, 0), Position = UDim2.new(0, 0, 0, 25), Text = "0"})
+	local closebutton = Screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0,25,0,25), TextXAlignment = Enum.TextXAlignment.Left, Text = "Close", BackgroundColor3 = Color3.new(1, 0, 0)})
+	local part3 = Screen:CreateElement("TextLabel", {TextScaled = true, Size = UDim2.new(0.1, 0, 0.1, 0), Position = UDim2.new(0.45, 0, 0, 25), Text = ""})
+	local part2 = Screen:CreateElement("TextLabel", {TextScaled = true, Size = UDim2.new(0.45, 0, 0.1, 0), Position = UDim2.new(0.55, 0, 0, 25), Text = ""})
+	holderframe:AddChild(part1)
+	holderframe:AddChild(part2)
+	holderframe:AddChild(part3)
+	holderframe:AddChild(closebutton)
+
+	local number1 = 0
+	local type = nil
+	local number2 = 0
+
+	local data = nil
+	local filename = nil
+
+	closebutton.MouseButton1Down:Connect(function()
+		holderframe:Destroy()
+	end)
+
+	local  button1 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.50, 0, 0.1, 25), Text = "9"})
+	holderframe:AddChild(button1)
+	button1.MouseButton1Down:Connect(function()
+		if not type then
+			number1 = tonumber(tostring(number1)..tostring(9))
+			part1.Text = number1
+		else
+			number2 = tonumber(tostring(number2)..tostring(9))
+			part2.Text = number2
+		end
+	end)
+
+	local  button2 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.25, 0, 0.1, 25), Text = "8"})
+	holderframe:AddChild(button2)
+	button2.MouseButton1Down:Connect(function()
+		if not type then
+			number1 = tonumber(tostring(number1)..tostring(8))
+			part1.Text = number1
+		else
+			number2 = tonumber(tostring(number2)..tostring(8))
+			part2.Text = number2
+		end
+	end)
+
+	local  button3 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0, 0, 0.1, 25), Text = "7"})
+	holderframe:AddChild(button3)
+	button3.MouseButton1Down:Connect(function()
+		if not type then
+			number1 = tonumber(tostring(number1)..tostring(7))
+			part1.Text = number1
+		else
+			number2 = tonumber(tostring(number2)..tostring(7))
+			part2.Text = number2
+		end
+	end)
+
+	local  button4 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.50, 0, 0.2, 25), Text = "6"})
+	holderframe:AddChild(button4)
+	button4.MouseButton1Down:Connect(function()
+		if not type then
+			number1 = tonumber(tostring(number1)..tostring(6))
+			part1.Text = number1
+		else
+			number2 = tonumber(tostring(number2)..tostring(6))
+			part2.Text = number2
+		end
+	end)
+	
+	local  button5 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.25, 0, 0.2, 25), Text = "5"})
+	holderframe:AddChild(button5)
+	button5.MouseButton1Down:Connect(function()
+		if not type then
+			number1 = tonumber(tostring(number1)..tostring(5))
+			part1.Text = number1
+		else
+			number2 = tonumber(tostring(number2)..tostring(5))
+			part2.Text = number2
+		end
+	end)
+
+	local  button6 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0, 0, 0.2, 25), Text = "4"})
+	holderframe:AddChild(button6)
+	button6.MouseButton1Down:Connect(function()
+		if not type then
+			number1 = tonumber(tostring(number1)..tostring(4))
+			part1.Text = number1
+		else
+			number2 = tonumber(tostring(number2)..tostring(4))
+			part2.Text = number2
+		end
+	end)
+	
+	local  button7 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.50, 0, 0.3, 25), Text = "3"})
+	holderframe:AddChild(button7)
+	button7.MouseButton1Down:Connect(function()
+		if not type then
+			number1 = tonumber(tostring(number1)..tostring(3))
+			part1.Text = number1
+		else
+			number2 = tonumber(tostring(number2)..tostring(3))
+			part2.Text = number2
+		end
+	end)
+
+	local  button8 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.25, 0, 0.3, 25), Text = "2"})
+	holderframe:AddChild(button8)
+	button8.MouseButton1Down:Connect(function()
+		if not type then
+			number1 = tonumber(tostring(number1)..tostring(2))
+			part1.Text = number1
+		else
+			number2 = tonumber(tostring(number2)..tostring(2))
+			part2.Text = number2
+		end
+	end)
+
+	local  button9 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0, 0, 0.3, 25), Text = "1"})
+	holderframe:AddChild(button9)
+	button9.MouseButton1Down:Connect(function()
+		if not type then
+			number1 = tonumber(tostring(number1)..tostring(1))
+			part1.Text = number1
+		else
+			number2 = tonumber(tostring(number2)..tostring(1))
+			part2.Text = number2
+		end
+	end)
+	
+	local  button10 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.25, 0, 0.4, 25), Text = "0"})
+	holderframe:AddChild(button10)
+	button10.MouseButton1Down:Connect(function()
+		if not type then
+			if tostring(number1) ~= "0" then
+				if tonumber(tostring(number1).."0") then
+					number1 = tostring(number1).."0"
+					part1.Text = number1
+				end
+			else
+				number1 = 0
+				part1.Text = number1
+			end
+		else
+			if tostring(number2) ~= "0" then
+				if tonumber(tostring(number2).."0") then
+					number2 = tostring(number2).."0"
+					part2.Text = number2
+				end
+			else
+				number2 = 0
+				part2.Text = number2
+			end
+		end
+	end)
+
+	local  button19 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.50, 0, 0.4, 25), Text = "."})
+	holderframe:AddChild(button19)
+	button19.MouseButton1Down:Connect(function()
+		if not type then
+			number1 = string.gsub(tostring(number1), "%.", "")
+			number1 = tostring(number1).."."
+			part1.Text = number1
+		else
+			number2 = string.gsub(tostring(number2), "%.", "")
+			number2 = tostring(number2).."."
+			part2.Text = number2
+		end
+	end)
+
+	local  button20 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.50, 0, 0.5, 25), Text = "(-)"})
+	holderframe:AddChild(button20)
+	button20.MouseButton1Down:Connect(function()
+		if not type then
+			number1 = string.gsub(tostring(number1), "-", "")
+			number1 = "-"..tostring(number1)
+			part1.Text = number1
+		else
+			number2 = string.gsub(tostring(number2), "-", "")
+			number2 = "-"..tostring(number2)
+			part2.Text = number2
+		end
+	end)
+
+	local  button11 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0, 0, 0.4, 25), Text = "CE"})
+	holderframe:AddChild(button11)
+	button11.MouseButton1Down:Connect(function()
+		number1 = 0
+		part1.Text = number1
+		number2 = 0
+		part2.Text = ""
+		type = nil
+		part3.Text = ""
+	end)
+
+	local  button12 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.75, 0, 0.1, 25), Text = "+"})
+	holderframe:AddChild(button12)
+	button12.MouseButton1Down:Connect(function()
+		type = "+"
+		part3.Text = "+"
+	end)
+
+	local  button13 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.75, 0, 0.2, 25), Text = "-"})
+	holderframe:AddChild(button13)
+	button13.MouseButton1Down:Connect(function()
+		type = "-"
+		part3.Text = "-"
+	end)
+
+	local  button14 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.75, 0, 0.3, 25), Text = "*"})
+	holderframe:AddChild(button14)
+	button14.MouseButton1Down:Connect(function()
+		type = "*"
+		part3.Text = "*"
+	end)
+
+	local  button15 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.75, 0, 0.4, 25), Text = "/"})
+	holderframe:AddChild(button15)
+	button15.MouseButton1Down:Connect(function()
+		type = "/"
+		part3.Text = "/"
+	end)
+
+	local  button17 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0, 0, 0.5, 25), Text = "√", RichText = true})
+	holderframe:AddChild(button17)
+	button17.MouseButton1Down:Connect(function()
+		type = "√"
+		part3.Text = "√"
+	end)
+
+	local  button18 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.25, 0, 0.5, 25), Text = "^", RichText = true})
+	holderframe:AddChild(button18)
+	button18.MouseButton1Down:Connect(function()
+		type = "^"
+		part3.Text = "^"
+	end)
+
+	local  button16 = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0.25, 0, 0.1, 0), Position = UDim2.new(0.75, 0, 0.5, 25), Text = "="})
+	holderframe:AddChild(button16)
+	button16.MouseButton1Down:Connect(function()
+		if type == "+" then
+			part1.Text = tonumber(number1) + tonumber(number2)
+			number1 = tonumber(number1) + tonumber(number2)
+			part2.Text = ""
+			number2 = 0
+			part3.Text = ""
+			type = nil
+		end
+		
+		if type == "-" then
+			part1.Text = tonumber(number1) - tonumber(number2)
+			number1 = tonumber(number1) - tonumber(number2)
+			part2.Text = ""
+			number2 = 0
+			part3.Text = ""
+			type = nil
+		end
+
+		if type == "*" then
+			part1.Text = tonumber(number1) * tonumber(number2)
+			number1 = tonumber(number1) * tonumber(number2)
+			part2.Text = ""
+			number2 = 0
+			part3.Text = ""
+			type = nil
+		end
+
+		if type == "/" then
+			part1.Text = tonumber(number1) / tonumber(number2)
+			number1 = tonumber(number1) / tonumber(number2)
+			part2.Text = ""
+			number2 = 0
+			part3.Text = ""
+			type = nil
+		end
+			
+		if type == "√" then
+			part1.Text = tonumber(number2) ^ (1 / tonumber(number1))
+			number1 = tonumber(number2) ^ (1 / tonumber(number1))
+			part2.Text = ""
+			number2 = 0
+			part3.Text = ""
+			type = nil
+		end
+			
+		if type == "^" then
+			part1.Text = tonumber(number1) ^ tonumber(number2)
+			number1 = tonumber(number1) ^ tonumber(number2)
+			part2.Text = ""
+			number2 = 0
+			part3.Text = ""
+			type = nil
+		end
+	end)
