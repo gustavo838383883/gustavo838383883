9c6c19e737bf1bf2256484f61555d820e63def30
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-10-30 17:09:14 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -5,9 +5,99 @@ local SpeakerHandler = {
 	DefaultSpeaker = nil,
 }
 
-function SpeakerHandler.CreateSound(config: { Id: number, Pitch: number, Length: number, Speaker: any } )
-	config.Pitch = config.Pitch or 1
+function SpeakerHandler.Chat(text, cooldownTime, speaker)
+	speaker = speaker or SpeakerHandler.DefaultSpeaker or error("[SpeakerHandler.Chat]: No speaker provided")
 
+	if SpeakerHandler._ChatCooldowns[speaker.GUID..text] then
+		return
+	end
+
+	speaker:Chat(text)
+
+	if not cooldownTime then
+		return
+	end
+
+	SpeakerHandler._ChatCooldowns[speaker.GUID..text] = true
+	task.delay(cooldownTime, function()
+		SpeakerHandler._ChatCooldowns[speaker.GUID..text] = nil
+	end)
+end
+
+function SpeakerHandler.PlaySound(id, pitch, cooldownTime, speaker)
+	speaker = speaker or SpeakerHandler.DefaultSpeaker or error("[SpeakerHandler.PlaySound]: No speaker provided")
+	id = tonumber(id)
+	pitch = tonumber(pitch) or 1
+
+	if SpeakerHandler._SoundCooldowns[speaker.GUID..id] then
+		return
+	end
+
+	speaker:Configure({Audio = id, Pitch = pitch})
+	speaker:Trigger()
+
+	if cooldownTime then
+		SpeakerHandler._SoundCooldowns[speaker.GUID..id] = true
+
+		task.delay(cooldownTime, function()
+			SpeakerHandler._SoundCooldowns[speaker.GUID..id] = nil
+		end)
+	end
+end
+
+function SpeakerHandler:LoopSound(id, soundLength, pitch, speaker)
+	speaker = speaker or SpeakerHandler.DefaultSpeaker or error("[SpeakerHandler:LoopSound]: No speaker provided")
+	id = tonumber(id)
+	pitch = tonumber(pitch) or 1
+	
+	if SpeakerHandler._LoopedSounds[speaker.GUID] then
+		SpeakerHandler:RemoveSpeakerFromLoop(speaker)
+	end
+	
+	speaker:Configure({Audio = id, Pitch = pitch})
+	
+	SpeakerHandler._LoopedSounds[speaker.GUID] = {
+		Speaker = speaker,
+		Length = soundLength / pitch,
+		TimePlayed = tick()
+	}
+	
+	speaker:Trigger()
+	return true
+end
+
+function SpeakerHandler:RemoveSpeakerFromLoop(speaker)
+	SpeakerHandler._LoopedSounds[speaker.GUID] = nil
+	
+	speaker:Configure({Audio = 0, Pitch = 1})
+	speaker:Trigger()
+end
+
+function SpeakerHandler:UpdateSoundLoop(dt) -- Triggers any speakers if it's time for them to be triggered
+	dt = dt or 0
+	
+	for _, sound in pairs(SpeakerHandler._LoopedSounds) do
+		local currentTime = tick() - dt
+		local timePlayed = currentTime - sound.TimePlayed
+
+		if timePlayed >= sound.Length then
+			sound.TimePlayed = tick()
+			sound.Speaker:Trigger()
+		end
+	end
+end
+
+function SpeakerHandler:StartSoundLoop() -- If you use this, you HAVE to put it at the end of your code.
+	
+	while true do
+		local dt = task.wait()
+		SpeakerHandler:UpdateSoundLoop(dt)
+	end
+end
+
+function SpeakerHandler.CreateSound(config: { Id: number, Pitch: number, Length: number, Speaker: any } ) -- Psuedo sound object, kinda bad
+	config.Pitch = config.Pitch or 1
+	
 	local sound = {
 		ClassName = "SpeakerHandler.Sound",
 		Id = config.Id,
@@ -16,41 +106,49 @@ function SpeakerHandler.CreateSound(config: { Id: number, Pitch: number, Length:
 		_OnCooldown = false, -- For sound cooldowns
 		_Looped = false
 	}
-
+	
 	if config.Length then
 		sound.Length = config.Length / config.Pitch
 	end
-
+	
 	function sound:Play(cooldownSeconds)
 		if sound._OnCooldown then
 			return
 		end
-
+		
 		sound._Speaker:Configure({Audio = sound.Id, Pitch = sound.Pitch})
 		sound._Speaker:Trigger()
-
+		
 		if not cooldownSeconds then
 			return
 		end
-
+		
 		sound._OnCooldown = true
 		task.delay(cooldownSeconds, function()
 			sound._OnCooldown = false
 		end)
 	end
-
+	
 	function sound:Stop()
 		sound._Speaker:Configure({Audio = 0, Pitch = 1})
 		sound._Speaker:Trigger()
-
+		
 		sound._OnCooldown = false
 	end
-
-
+	
+	function sound:Loop()
+		sound._Looped = true
+		SpeakerHandler:LoopSound(sound.Id, sound.Length, sound.Pitch, sound._Speaker)
+	end
+	
 	function sound:Destroy()
+		if sound._Looped then
+			SpeakerHandler:RemoveSpeakerFromLoop(sound._Speaker)
+		end
+		
 		table.clear(sound)
 	end
-
+	
 	return sound
 end
 
@@ -382,7 +480,7 @@ local function woshtmlfile(txt, screen, boolean)
 
 end
 
-local function audioui(screen, disk, data, speaker, pitch)
+local function audioui(screen, disk, data, speaker, pitch, length)
 	local holderframe = screen:CreateElement("Frame", {Size = UDim2.new(0.5, 0, 0.5, 0), Active = true, Draggable = true})
 	local closebutton = screen:CreateElement("TextButton", {TextScaled = true, Size = UDim2.new(0,25,0,25), TextXAlignment = Enum.TextXAlignment.Left, Text = "Close", BackgroundColor3 = Color3.new(1, 0, 0)})
 	holderframe:AddChild(closebutton)
@@ -405,7 +503,13 @@ local function audioui(screen, disk, data, speaker, pitch)
 		Id = tonumber(data),
 		Pitch = tonumber(pitch),
 		Speaker = speaker,
+		Length = length
 	})
+
+	if length then
+		sound:Loop()
+	end
+	
 	sound:Play()
 
 	
@@ -455,11 +559,20 @@ local function readfile(txt, nameondisk, boolean)
 	
 	if string.find(string.lower(tostring(nameondisk)), ".aud") then
 		if string.find(tostring(txt), "pitch:") then
+			local length = nil
+			
 			local splitted = string.split(tostring(txt), "pitch:")
-			audioui(screen, disk, splitted[1], speaker, tonumber(splitted[2]))
+			
+			if string.find(tostring(txt), "length:") then
+				local splitted = string.split(tostring(txt), "length:")
+				length = splitted[2]
+			end
+			
+			audioui(screen, disk, splitted[1], speaker, tonumber(splitted[2]), length)
+			
 		elseif string.find(tostring(txt), "length:") then
 			local splitted = string.split(tostring(txt), "length:")
-			audioui(screen, disk, splitted[1], speaker)
+			audioui(screen, disk, splitted[1], speaker, nil, tonumber(splitted[2]))
 		else
 			audioui(screen, disk, txt, speaker)
 		end
@@ -1021,15 +1134,20 @@ local function mediaplayer(screen, disk, speaker)
 		if Filename.Text ~= "File with id (Click to update)" then
 			local data = disk:Read(data)
 			if string.find(tostring(data), "pitch:") then
-					
+				local length = nil
+				
 				local splitted = string.split(tostring(data), "pitch:")
-				audioui(screen, disk, splitted[1], speaker, tonumber(splitted[2]))
-					
+				
+				if string.find(tostring(data), "length:") then
+					local splitted = string.split(tostring(data), "length:")
+					length = splitted[2]
+				end
+				
+				audioui(screen, disk, splitted[1], speaker, tonumber(splitted[2]), length)
+				
 			elseif string.find(tostring(data), "length:") then
-					
 				local splitted = string.split(tostring(data), "length:")
-				audioui(screen, disk, splitted[1], speaker)
-					
+				audioui(screen, disk, splitted[1], speaker, nil, tonumber(splitted[2]))
 			else
 				audioui(screen, disk, data, speaker)
 			end
