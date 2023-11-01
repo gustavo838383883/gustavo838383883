6ace6094cdb94bae60d7228264f46d735201711c
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-11-01 13:52:13 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -708,13 +708,12 @@ local function readfile(txt, nameondisk, boolean)
 		local start = 0
 		local holderframe = screen:CreateElement("TextButton", {Size = UDim2.new(0.7, 0, 0.7, 0), Active = true, Draggable = true, TextTransparency = 1})
 		programholder1:AddChild(holderframe)
-		local scrollingframe = screen:CreateElement("ScrollingFrame", {Size = UDim2.new(1, 0, 1, -25), CanvasSize = UDim2.new(0, 0, 0, 0), Position = UDim2.new(0, 0, 0, 25)})
+		local scrollingframe = screen:CreateElement("ScrollingFrame", {Size = UDim2.new(1, -75, 0, 25), CanvasSize = UDim2.new(0, 0, 0, 0), Position = UDim2.new(0, 0, 0, 25)})
 		holderframe:AddChild(scrollingframe)
 		local textlabel = screen:CreateElement("TextLabel", {TextScaled = true, Size = UDim2.new(1,-50,0,25), Position = UDim2.new(0, 50, 0, 0), TextXAlignment = Enum.TextXAlignment.Left, Text = "Table Content"})
 		holderframe:AddChild(textlabel)
 		
 		if boolean == true then
-			textlabel.Size = UDim2.new(1, -75, 0, 25)
 			
 			local deletebutton = screen:CreateElement("TextButton", {Size = UDim2.new(0, 25, 0, 25),Position = UDim2.new(1, -25, 0, 0), Text = "Delete", TextScaled = true})
 			holderframe:AddChild(deletebutton)
