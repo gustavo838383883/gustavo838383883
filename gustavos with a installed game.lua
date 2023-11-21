27e2bdda83a23d862e6f63f76d73cd69d2e3f7fb
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-11-21 14:21:17 -0300

    Update gustavos with a installed game.lua

--- gustavos with a installed game.lua
+++ gustavos with a installed game.lua
@@ -1475,10 +1475,10 @@ local function gameload()
 	
 	while task.wait(0.01) do
 		if GetCollidedGuiObjects(hitbox, lavas) then
-		plr.Position = UDim2.new(0,0,0,0)
-		thegame.Position = UDim2.new(0.5, -25, 0.5, -25)
-		speaker:PlaySound("rbxassetid://3802269741")
-	end
+			plr.Position = UDim2.new(0,0,0,0)
+			thegame.Position = UDim2.new(0.5, -25, 0.5, -25)
+			speaker:PlaySound("rbxassetid://3802269741")
+		end
 		
 		if plr.Position.Y.Offset > 150 then
 			plr.Position = UDim2.new(0,0,0,0)
