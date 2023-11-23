05b3dfeeffd09c70cf3fe65586aa5bb9281eaf08
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-11-23 14:48:12 -0300

    Update syq but with terrain generator.lua

--- syq but with terrain generator.lua
+++ syq but with terrain generator.lua
@@ -344,7 +344,9 @@ for i=1, 5000 do
 	local grass = screen:CreateElement("ImageLabel", {Image = "rbxassetid://11693507606", Size = UDim2.new(0, 25, 0, 25), Position = UDim2.new(0, start, 0, y), BackgroundTransparency = 1})
 	local randomnumber = math.random(1, 3)
 
-	ground:AddChild(grass)
+	if grass then
+		ground:AddChild(grass)
+	end
 	
 	if randomnumber == 2 then
 		y -= 25
