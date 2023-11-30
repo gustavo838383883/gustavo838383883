6122130e96619668704ec3230b2866a8566dacb9
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-11-30 15:32:41 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1170,8 +1170,8 @@ local function chatthing(screen, disk, modem)
 		
 		sendbox.MouseButton1Up:Connect(function()
 			if keyboardinput then
-				sendbox.Text = keyboardinput:gsub(".?$","");
-				sendtext = keyboardinput:gsub(".?$","");
+				sendbox.Text = keyboardinput
+				sendtext = keyboardinput
 				player = playerthatinputted
 			end
 		end)
@@ -1875,7 +1875,7 @@ local function loadmenu(screen, disk)
 										keyboardevent = nil
 									end
 									keyboardevent = keyboard:Connect("TextInputted", function(text, plr)
-										keyboardinput = text
+										keyboardinput = text:gsub(".?$","");
 										playerthatinputted = plr
 									end)
 								else
@@ -1969,7 +1969,7 @@ function startload()
 						keyboardevent = nil
 					end
 					keyboardevent = keyboard:Connect("TextInputted", function(text, plr)
-						keyboardinput = text
+						keyboardinput = text:gsub(".?$","");
 						playerthatinputted = plr
 					end)
 					if disk:Read("BackgroundImage") or disk:Read("BackgroundColor") or disk:Read("sounds") then
