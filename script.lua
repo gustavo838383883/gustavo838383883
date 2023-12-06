2749332557df29cfa3cfb56f88c41300d5722bf3
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-12-06 13:34:20 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1151,8 +1151,7 @@ local function writedisk(screen, disk)
 			if split then
 				print(inputtedtext)
 				print(split)
-				print(-split[#split]-1)
-				local removedlast = inputtedtext:sub(1, -split[#split]-1)
+				local removedlast = inputtedtext:sub(1, -#split-1)
 				print(removedlast)
 				if getfilefromtable(disk, split[#split], removedlast) then
 					directorybutton.Text = inputtedtext
