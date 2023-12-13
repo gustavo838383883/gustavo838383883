69b10dd25a9a1a5264a0243a37242e8e8461e82f
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-12-13 16:19:17 -0300

    Update gustavosdesktop7.lua

--- gustavosdesktop7.lua
+++ gustavosdesktop7.lua
@@ -579,6 +579,8 @@ local function loaddesktop()
 	programholder1 = screen:CreateElement("Frame", {Size = UDim2.new(1, 0, 1, 0), BackgroundTransparency = 1})
 	programholder2 = screen:CreateElement("Frame", {Size = UDim2.new(1, 0, 1, 0), BackgroundTransparency = 1})
 	programholder2:AddChild(programholder1)
+
+	taskbarholder = screen:CreateElement("ImageButton", {Image = "rbxassetid://15619032563", Position = UDim2.new(0.1, 0, 0.9, 0), Size = UDim2.new(0.9, 0, 0.1, 0), BackgroundTransparency = 1, ImageTransparency = 0.2})
 	
 	local pressed = false
 	local startmenu
