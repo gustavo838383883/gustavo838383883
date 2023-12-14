4a891633164830179f03a166fa9197ba3fb6e517
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-12-13 21:06:55 -0300

    Update gustavosdesktop7.lua

--- gustavosdesktop7.lua
+++ gustavosdesktop7.lua
@@ -744,6 +744,7 @@ local function loaddesktop()
 					task.wait(1)
 					speaker:ClearSounds()
 					speaker:PlaySound("rbxassetid://7762841318")
+					backgroundcolor.BackgroundTransparency = 1
 					for i=0,1,0.05 do
 						task.wait(0.05)
 						wallpaper.ImageTransparency = i
