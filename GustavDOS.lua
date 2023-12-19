9e6c410347fb8d6daaeea23c96cda4323b2dfc74
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-12-19 17:38:10 -0300

    Update GustavDOS.lua

--- GustavDOS.lua
+++ GustavDOS.lua
@@ -1018,12 +1018,12 @@ local function runtext(text)
 			end
 			if not split or split[2] == "" then
 				local textlabel = commandlines:insert(tostring(disk:Read(filename)), UDim2.fromOffset(screen:GetDimensions().X, screen:GetDimensions().Y))
-				StringToGui(screen, [[<img src="]]..tonumber(disk:Read(filename))..[[" size="1,0,1,0">]], textlabel)
+				StringToGui(screen, [[<img src="]]..tonumber(disk:Read(filename))..[[" size="1,0,1,0" position="0,0,0,0">]], textlabel)
 				textlabel:AddChild(imagelabel)
 				background.CanvasPosition = Vector2.new(0, commandlines.number.Y.Offset - 25)
 			else
 				local textlabel = commandlines:insert(tostring(getfileontable(disk, filename, dir)), UDim2.fromOffset(screen:GetDimensions().X, screen:GetDimensions().Y))
-				StringToGui(screen, [[<img src="]]..tonumber(disk:Read(filename))..[[" size="1,0,1,0">]], textlabel)
+				StringToGui(screen, [[<img src="]]..tonumber(disk:Read(filename))..[[" size="1,0,1,0" position="0,0,0,0">]], textlabel)
 				background.CanvasPosition = Vector2.new(0, commandlines.number.Y.Offset - 25)
 			end
 		else
