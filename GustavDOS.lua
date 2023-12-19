477b5f3b4a860cc382ecf38ed0fc1a099fc0aaaa
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-12-19 15:58:27 -0300

    Update GustavDOS.lua

--- GustavDOS.lua
+++ GustavDOS.lua
@@ -972,12 +972,11 @@ local function runtext(text)
 			if dir ~= "" then
 				split = string.split(dir, "/")
 			end
-			if not split or split[2] == "" then
-				disk:Write(filename, nil)
-			else
+			if split and split[2] ~= "" then
 				returntable = createfileontable(disk, filename, nil, dir)
 			end
 			if not split or split[2] == "" then
+				disk:Write(filename, nil)
 				if not disk:Read(filename) then
 					commandlines:insert("Success i think")
 				else
