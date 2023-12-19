2bfa00d0b27d1f5d0bfe2bc7aef777a16454e922
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-12-19 13:10:06 -0300

    Update GustavDOS.lua

--- GustavDOS.lua
+++ GustavDOS.lua
@@ -438,7 +438,7 @@ function commandline.new(screen, udim2)
 	function lines:insert(text)
 		local textlabel = screen:CreateElement("TextLabel", {BackgroundTransparency = 1, TextColor3 = Color3.new(1,1,1), Text = tostring(text), TextScaled = true, TextWrapped = true, TextXAlignment = Enum.TextXAlignment.Left, Size = UDim2.new(1, 0, 0, 25), Position = lines.number})
 		background:AddChild(textlabel)
-		background.CanvasSize = UDim2.new(1, 0, lines.number.Y.Scale, lines.number.Y.Offset + UDim2.fromOffset(0, 25))
+		background.CanvasSize = UDim2.new(1, 0, lines.number.Y.Scale, lines.number.Y.Offset + 25)
 		if typeof(udim2) == "UDim2" then
 			textlabel.Size = udim2
 			background.CanvasSize -= UDim2.fromOffset(0, 25)
