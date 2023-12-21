c69ffab1d28aa884dad9c093ff879c202cd98d7c
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2023-12-21 00:02:16 -0300

    Update GustavDOS.lua

--- GustavDOS.lua
+++ GustavDOS.lua
@@ -437,7 +437,7 @@ function commandline.new(screen)
 	local background = screen:CreateElement("ScrollingFrame", {Size = UDim2.new(1, 0, 1, 0), BackgroundColor3 = Color3.new(0,0,0), ScrollBarThickness = 5})
 	local lines = {
 		insert = function(text, udim2)
-			local textlabel = screen:CreateElement("TextLabel", {BackgroundTransparency = 1, TextColor3 = Color3.new(1,1,1), Text = tostring(text), TextScaled = true, RichText = true, TextWrapped = true, TextXAlignment = Enum.TextXAlignment.Left, TextYAlignment = Enum.TextYAlignment.Top, Size = UDim2.new(1, 0, 0, 25), Position = lines.number})
+			local textlabel = screen:CreateElement("TextLabel", {BackgroundTransparency = 1, TextColor3 = Color3.new(1,1,1), Text = tostring(text), TextScaled = true, RichText = true, TextWrapped = true, TextXAlignment = Enum.TextXAlignment.Left, TextYAlignment = Enum.TextYAlignment.Top, Size = UDim2.new(1, 0, 0, 25), Position = position})
 			if textlabel then
 				background:AddChild(textlabel)
 				background.CanvasSize = UDim2.new(1, 0, 0, position.Y.Offset + 25)
