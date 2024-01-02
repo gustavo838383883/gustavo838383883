b451ed8b3aee1fd9095051cdf40a18bec1778edf
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2024-01-02 13:12:22 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1266,7 +1266,7 @@ local function chatthing(screen, disk, modem)
 		local scrollingframe = screen:CreateElement("ScrollingFrame", {ScrollBarThickness = 5, Size = UDim2.new(1, 0, 0.8, -25), Position = UDim2.new(0, 0, 0.1, 25), BackgroundTransparency = 1})
 		holderframe:AddChild(scrollingframe)
 	
-		local sendbox =  screen:CreateElement("TextButton", {Size = UDim2.new(0.8, 0, 0.1, 0), Position = UDim2.new(0,0,0.9,0), Text = "Message (Click to update)", TextScaled = true})
+		local sendbox =  screen:CreateElement("TextButton", {RichText = true, Size = UDim2.new(0.8, 0, 0.1, 0), Position = UDim2.new(0,0,0.9,0), Text = "Message (Click to update)", TextScaled = true})
 		holderframe:AddChild(sendbox)
 	
 		local sendtext = nil
