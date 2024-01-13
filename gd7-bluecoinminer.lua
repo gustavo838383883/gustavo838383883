2edf0a0d146a2f4d66c83950a13f5596b41863b1
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2024-01-12 21:23:42 -0300

    Update gd7-bluecoinminer.lua

--- gd7-bluecoinminer.lua
+++ gd7-bluecoinminer.lua
@@ -1,6 +1,6 @@
 --og bluecoin atm made by blueloops9
 --gui made by Gustavo12345687890 / Gustavo242
---reskin of the puter bluecoin atm for gd7
+--reskin of the puter bluecoin atm for made by robloxboxertBLOCKED / 0mori2
 --made in 2024
 
 local prevCursorPos
@@ -59,15 +59,15 @@ if tonumber(clicksound) then clicksound = "rbxassetid://"..clicksound end
 local screen = Screen
 
 function CreateWindow(udim2, title, boolean, boolean2, boolean3, text, boolean4)
-    local udim2 = udim2 + UDim2.new(0, 0, 0, defaultbuttonsize.Y + (defaultbuttonsize.Y/2))
-    local holderframe = screen:CreateElement("ImageButton", {ClipsDescendants = true, Size = udim2, BackgroundTransparency = 1, Image = "rbxassetid://8677487226", ImageTransparency = 0.2})
-    if not holderframe then return end
-    if programholder1 then
-        programholder1:AddChild(holderframe)
-    end
-    if not gputer["Screen"] then
-        holderframe.ZIndex = 3
-    end
+	local udim2 = udim2 + UDim2.new(0, 0, 0, defaultbuttonsize.Y + (defaultbuttonsize.Y/2))
+	local holderframe = screen:CreateElement("ImageButton", {ClipsDescendants = true, Size = udim2, BackgroundTransparency = 1, Image = "rbxassetid://8677487226", ImageTransparency = 0.2})
+	if not holderframe then return end
+	if programholder1 then
+	programholder1:AddChild(holderframe)
+	end
+	if not gputer["Screen"] then
+		holderframe.ZIndex = 3
+	end
 	local textlabel
 	if typeof(title) == "string" then
 		textlabel = screen:CreateElement("TextLabel", {Size = UDim2.new(1, -(defaultbuttonsize.X*2), 0, defaultbuttonsize.Y), BackgroundTransparency = 1, Position = UDim2.new(0, defaultbuttonsize.X*2, 0, 0), TextScaled = true, TextWrapped = true, Text = tostring(title)})
