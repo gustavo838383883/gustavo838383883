3e82858eef8e7a1db3123ad0c932fce8ac5940b4
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2024-01-15 13:20:12 -0300

    Update gustavosdesktop7.lua

--- gustavosdesktop7.lua
+++ gustavosdesktop7.lua
@@ -1398,6 +1398,7 @@ local function readfile(txt, nameondisk, boolean, directory)
 		end
 
 		refreshbutton.MouseButton1Up:Connect(function()
+			start = 0
 			scrollingframe:Destroy()
 			scrollingframe = screen:CreateElement("ScrollingFrame", {ScrollBarThickness = 5, Size = UDim2.new(1, 0, 0.9, 0), CanvasSize = UDim2.new(0, 0, 0, 0), Position = UDim2.new(0, 0, 0.1, 0), BackgroundTransparency = 1})
 			holderframe:AddChild(scrollingframe)
@@ -1441,6 +1442,7 @@ local function loaddisk()
 	end
 
 	refreshbutton.MouseButton1Up:Connect(function()
+		start = 0
 		scrollingframe:Destroy()
 		scrollingframe = screen:CreateElement("ScrollingFrame", {ScrollBarThickness = 5, Size = UDim2.new(1, 0, 0.9, 0), CanvasSize = UDim2.new(0, 0, 0, 0), Position = UDim2.new(0, 0, 0.1, 0), BackgroundTransparency = 1})
 		holderframe:AddChild(scrollingframe)
