bdddf1d5d301b348c6668054c1176eac7f96ef7b
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2024-01-14 23:28:40 -0300

    Update gustavosdesktop7.lua

--- gustavosdesktop7.lua
+++ gustavosdesktop7.lua
@@ -1338,7 +1338,7 @@ local function readfile(txt, nameondisk, boolean, directory)
 		
 		local tableval = txt
 		local start = 0
-		local holderframe, window, closebutton, maximizebutton, textlabel = CreateWindow(UDim2.new(0.7, 0, 0.7, 0), directory, false, false, false, "Table Content", false)
+		local holderframe, window, closebutton, maximizebutton, textlabel = CreateWindow(UDim2.new(0.7, 0, 0.7, 0), newdirectory, false, false, false, "Table Content", false)
 		local scrollingframe = screen:CreateElement("ScrollingFrame", {ScrollBarThickness = 5, Size = UDim2.new(1, 0, 1, 0), CanvasSize = UDim2.new(0, 0, 0, 0), Position = UDim2.new(0, 0, 0, 0), BackgroundTransparency = 1})
 		holderframe:AddChild(scrollingframe)
 		textlabel.Size -= UDim2.new(0, defaultbuttonsize.Y, 0, 0)
