f30c375d66c5cbba7f76e52afc044c6e937ed164
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2024-01-29 13:19:25 -0300

    Update GustavDOS.lua

--- GustavDOS.lua
+++ GustavDOS.lua
@@ -903,7 +903,8 @@ local function runtext(text)
 			if #split == 2 and split[2] == "" then
 				commandlines.insert("Cannot use ./ on root.")
 			else
-				dir = dir:sub(1, -(string.len(split[#split]))-2)
+				local newdir = dir:sub(1, -(string.len(split[#split]))-2)
+				dir = if newdir == "" then "/" else newdir
 			end
 		else
 			commandlines.insert("The table/folder name was not specified.")
