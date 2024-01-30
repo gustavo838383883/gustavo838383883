761de82bdc6571329c58c8312260feb4082e5baf
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2024-01-30 20:13:44 -0300

    Update NetworkId Browser Server.lua

--- NetworkId Browser Server.lua
+++ NetworkId Browser Server.lua
@@ -1,4 +1,5 @@
 local modem = GetPartFromPort(1, "Modem")
+local disk = GetPartFromPort(2, "Disk")
 local id = 20
 modem:Configure({NetworkId = id})
 
@@ -11,10 +12,41 @@ modem:Connect("MessageSent", function(text1)
 		local text = table1["Text"]
 
 		if mode == "SendMessage" then
-			local result = {["Mode"] = "ServerSend", ["Text"] = "Test is a success"}
-			for index, value in pairs(result) do print(value) end
-			task.wait(1)
-			modem:SendMessage(JSONEncode(result), id)
+			local command = if text then string.sub(text, 1, 6):lower() else ""
+			if command == "make, " then
+				local data = string.sub(text, 7, string.len(text))
+				
+				local s = string.find(data, "/")
+
+				local text1 = string.sub(data, 1, s-1)
+				local text2 = string.sub(data, s+1, string.len(data))
+
+				local returntext = "Failed"
+
+				if text1 and text2 then
+					disk:Write(text1, text2)
+				end
+
+				if disk:Read(text1) == text2 then returntext = "Success" end
+				
+				local result = {["Mode"] = "ServerSend", ["Text"] = returntext}
+				task.wait(1)
+				modem:SendMessage(JSONEncode(result), id)
+			elseif command == "read, " then
+				local data = string.sub(text, 7, string.len(text))
+
+				local returntext = ""
+
+				returntext = disk:Read(data) or "Failed"
+
+				local result = {["Mode"] = "ServerSend", ["Text"] = returntext}
+				task.wait(1)
+				modem:SendMessage(JSONEncode(result), id)
+			else
+				local result = {["Mode"] = "ServerSend", ["Text"] = "Invalid command"}
+				task.wait(1)
+				modem:SendMessage(JSONEncode(result), id)
+			end
 		end
 		
 	end
