632c79cad1506c466b5b4190302abd3cf650c8aa
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2024-01-30 16:36:25 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1099,7 +1099,7 @@ local function writedisk(screen, disk)
 	end)
 
 	createtablebutton.MouseButton1Down:Connect(function()
-		if filenamebutton.Text ~= "File Name(Case Sensitive if on a table) (Click to update)" and filename ~= "Color" and filename ~= "BackgroundImage" and filename ~= "GustavOS Library" then
+		if filenamebutton.Text ~= "File Name(Case Sensitive if on a table) (Click to update)" and filename ~= "Color" and filename ~= "BackgroundImage" and filename ~= "GustavOSLibrary" then
 			local split = nil
 			local returntable = nil
 			if directory ~= "" then
