eddeefca911f1cc0462d0b9109a07f91d8c46619
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2024-01-31 16:29:21 -0300

    Update NetworkID Browser server 2.lua

--- NetworkID Browser server 2.lua
+++ NetworkID Browser server 2.lua
@@ -89,7 +89,7 @@ modem:Connect("MessageSent", function(text1)
 				["Children"] = {
 					["ClassName"] = "TextLabel",
 					["Properties"] = {
-						["Size"] = udim2totable(UDim2.fromScale(1, 1)),
+						["Size"] = udim2totable(UDim2.fromScale(1, 0.5)),
 						["TextScaled"] = true,
 						["TextWrapped"] = true,
 						["BackgroundColor3"] = color3totable(Color3.new(1,1,1)),
