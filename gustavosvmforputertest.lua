e63e65e2228f9fe77184ed988deb5682c6801f9e
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2024-02-16 20:03:33 -0300

    Update gustavosvmforputertest.lua

--- gustavosvmforputertest.lua
+++ gustavosvmforputertest.lua
@@ -2230,7 +2230,7 @@ function startload()
 							end
 						end
 					end)
-					if disk:Read("BackgroundImage") or disk:Read("BackgroundColor") or disk:Read("sounds") then
+					if disk:Read("BackgroundImage") or disk:Read("Color") or disk:Read("sounds") then
 						loadmenu()
 						Beep(0.25)
 						task.wait(0.1)
