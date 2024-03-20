83aca7c95db07c1c1af1e3e35df0d3179452cd0b
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2024-03-20 18:05:18 -0300

    Update sys but with terrain generator.lua

--- sys but with terrain generator.lua
+++ sys but with terrain generator.lua
@@ -274,7 +274,7 @@ plr:AddChild(hitbox)
 
 local allobjects = {}
 
-local loadingframe = screen:CreateElement("ImageLabel", {Size = UDim2.new(1,0,1,0), BackgroundColor3 = Color3.new(0,0,0), Image = "rbxassetid://15440219329", ScaleType = Enum.ScaleType.Tile, TileSize = UDim2.new(0.2, 0, 0.2, 0)})
+local loadingframe = screen:CreateElement("ImageLabel", {Size = UDim2.new(1,0,1,0), BackgroundColor3 = Color3.new(0,0,0), Image = "rbxassetid://15440219329", ScaleType = Enum.ScaleType.Tile, TileSize = UDim2.new(0.2, 0, 0.2, 0), ScaleType = Enum.ScaleType.Fit})
 window:AddChild(loadingframe)
 
 local loadingbar = screen:CreateElement("Frame", {Position = UDim2.new(0, 0, 0.4, 0), Size = UDim2.new(0, 0, 0.2, 0)})
