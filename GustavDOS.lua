ed2ec69233c87185e24bd7b3ae93c0e721872a52
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2024-05-09 16:59:07 -0300

    Update GustavDOS.lua

--- GustavDOS.lua
+++ GustavDOS.lua
@@ -155,7 +155,7 @@ end
 local function createfileontable(disk, filename, filedata, directory)
 	local returntable = nil
 	local directory = directory
-	if directory:sub(-1, -1) == "/" then directory = directory:sub(0, -2) end
+	if directory:sub(-1, -1) == "/" then directory = directory:sub(1, -2) end
 	local split = string.split(directory, "/")
 
 	if split then
@@ -187,7 +187,7 @@ end
 
 local function getfileontable(disk, filename, directory)
 	local directory = directory
-	if directory:sub(-1, -1) == "/" then directory = directory:sub(0, -2) end
+	if directory:sub(-1, -1) == "/" then directory = directory:sub(1, -2) end
 	local split = string.split(directory, "/")
 	local file = nil
 	if split then
