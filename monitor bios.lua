a1079aa1b2e58764f185505e220fc3318b6a32ac
gustavo838383883 <104228364+gustavo838383883@users.noreply.github.com>
2024-10-12 21:31:39 -0300

    Update monitor bios.lua

--- monitor bios.lua
+++ monitor bios.lua
@@ -1,4 +1,49 @@
 local ts = GetPart("TouchScreen") or GetPart("Screen")
+local speaker = GetPart("Speaker")
+local upport = GetPort(1)
+local downport = GetPort(2)
+local hasvolumecontrol = false
+
+if upport and downport and speaker then
+	hasvolumecontrol = true
+end
+
+local volumeframe
+local volumebar
+
+local function createvolumegui()
+	volumeframe = screen:CreateElement("Frame", {BackgroundColor3 = Color3.new(0, 0, 1), Size = UDim2.new(0.5, 0, 0, 25), Position = UDim2.new(0.25, 0, 1, -50)})
+	local speakerimage = screen:CreateElement("ImageLabel", {BackgroundTransparency = 1, Image = "rbxassetid://83229244752624", Size = UDim2.fromOffset(25, 25), ResampleMode = Enum.ResamplerMode.Pixelated})
+	local minusimage = screen:CreateElement("ImageLabel", {BackgroundTransparency = 1, Image = "rbxassetid://105700715661994", Position = UDim2.fromOffset(25, 0) Size = UDim2.fromOffset(25, 25), ResampleMode = Enum.ResamplerMode.Pixelated})
+	local plusimage = screen:CreateElement("ImageLabel", {BackgroundTransparency = 1, Image = "rbxassetid://134501568804650", Position = UDim2.new(1, -25, 0, 0) Size = UDim2.fromOffset(25, 25), ResampleMode = Enum.ResamplerMode.Pixelated})
+	local barbackground = screen:CreateElement("Frame", {Size = UDim2.new(1, -75, 0.5, 0), BorderSizePixel = 0, Position = UDim2.new(0, 50, 0.25, 0), BackgroundColor3 = Color3.new(0, 0, 0})
+	volumebar = screen:CreateElement("Frame", {Size = UDim2.fromScale(speaker.Volume, 1), BorderSizePixel = 0, BackgroundColor3 = Color3.new(1, 1, 1})
+	speakerimage.Parent = volumeframe
+	minusimage.Parent = volumeframe
+	plusimage.Parent = volumeframe
+	barbackground.Parent = volumeframe
+	volumebar.Parent = barbackground
+end
+
+local pressedtick
+
+local function getportfunction(x)
+	return function()
+		pressedtick = tick()
+		if not volumeframe then
+			createvolumegui()
+		else
+			local newvolume = math.clamp(x + math.floor(speaker.Volume*10)/10, 0, 1)
+			speaker.Volume = newvolume
+			volumebar.Size = UDim2.fromScale(newvolume, 1)
+		end
+	end
+end
+
+upport.Triggered:Connect(getportfunction(0.1))
+
+downport.Triggered:Connect(getportfunction(-0.1))
+
 local imagelabel = nil
 local screencanvas = ts:GetCanvas()
 local screensize = ts:GetDimensions()
@@ -97,6 +142,13 @@ end
 local waittime = 1
 local main = false
 while true do
+	if volumeframe then
+		if tick() - pressedtick() > 2 then
+			volumeframe:Destroy()
+			volumeframe = nil
+			volumebar = nil
+		end
+	end
 	if main then
 		if exists(imagelabel) then
 			setdirection(imagelabel)
@@ -115,7 +167,7 @@ while true do
 		local empty, e = isscreenempty()
 		if empty then
 			if exists(e) then e:Destroy() end
-			imagelabel = ts:CreateElement("ImageLabel", {Image = "rbxassetid://123464338631816", Size = UDim2.fromOffset(68, 15.8), BackgroundColor3 = Color3.new(0, 0, 1), Position = UDim2.fromOffset(math.random(0, math.floor(maxx/20))*20, math.random(0, math.floor(maxy/20))*20)})
+			imagelabel = ts:CreateElement("ImageLabel", {ResampleMode = Enum.ResamplerMode.Pixelated, Image = "rbxassetid://123464338631816", Size = UDim2.fromOffset(68, 15.8), BackgroundColor3 = Color3.new(0, 0, 1), Position = UDim2.fromOffset(math.random(0, math.floor(maxx/20))*20, math.random(0, math.floor(maxy/20))*20)})
 			main = true
 			waittime = 0.25
 		end
